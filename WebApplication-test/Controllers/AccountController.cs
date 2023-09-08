6f283c6765934c77a2bb37d0c5f9b66c6248b727
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2023-09-09 07:21:37 +0800

    first commit

---
+++ WebApplication-test/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_test.Models.EntityManager;
+using WebApplication_test.Models.ViewModel;
+using WebApplication_test.Models;
+
+namespace WebApplication_test.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    // FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            var users = new UserManager().GetAllUsers();
+
+            return View();
+        }
+    }
+}
